1c92bd2c6a3567ce8fbfca82695254fa7e4b869c
Jesse Reiss <jreiss@oogalabs.com>
2011-05-15 18:16:40 -0700

    fix

--- app/controllers/site/sitemaps_controller.rb
+++ app/controllers/site/sitemaps_controller.rb
@@ -1,5 +1,5 @@
 class Site::SitemapsController < Site::BaseController
   def show
-    @posts = Post.paginate(:per_page => 10000)
+    @posts = Post.paginate(:per_page => 10000, :page => 1)
   end
 end
